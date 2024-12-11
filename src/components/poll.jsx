4e59b9909d8423e46c2e45008a1503d3c06c11b2
C-hetha-N <chethannchethann80@gmail.com>
2024-12-11 11:02:47 +0530

    Debuging Poll button

--- src/components/poll.jsx
+++ src/components/poll.jsx
@@ -185,8 +185,8 @@ const Poll = () => {
                   </motion.div>  
 
                   <motion.div
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
+                    // whileHover={{ scale: 1.05 }}
+                    // whileTap={{ scale: 0.95 }}
                     className="text-center"
       >
                       <button
